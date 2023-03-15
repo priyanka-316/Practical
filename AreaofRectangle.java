87cfb1f048e0034cd21cf2a30777bc0144d5a554
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-15 13:27:44 +0530

    Add files via upload

---
+++ AreaofRectangle.java
@@ -0,0 +1,19 @@
+class AreaofRectangle
+{
+ 
+     public static void main(String args[]){
+        
+         
+        int   len= 5;
+        int  bre=4;
+         int area=len*bre;
+        System.out.println("Area of Rectangle is: " + area);   
+  
+
+          int l=5, b=6;
+         int perimeter = 2 * (l + b);
+        System.out.println("Perimeter of rectangle:"+perimeter); 
+          
+       
+   }
+}
