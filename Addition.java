a1c3a5e2e89049355c2775aa79bc3605527cbc4b
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-15 13:27:14 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,66 @@
+class Addition
+{
+public static void main(String args[]){
+
+System.out.println("This is Addtiontion");
+
+System.out.println("Two number is ");
+int a = 5, b=6; // 
+System.out.println(a + " "+ b);
+int sum=a+b;
+System.out.println("The sum is ="+sum);
+
+
+System.out.println("This is Substraction");
+
+System.out.println("Two number is ");
+int c=20; // 1st local variable
+int d=11; // 2nd local variable
+System.out.println(c + " "+ d);
+int sub=c-d;
+System.out.println("The Sub is ="+sub);
+
+System.out.println("This is Multiplication");
+
+System.out.println("Two number is ");
+int e=5; // 1st local variable
+int f=10; // 2nd local variable
+System.out.println(e + " "+ f);
+int mul=e*f;
+System.out.println("The mul is ="+mul);
+
+System.out.println("This is Division");
+
+System.out.println("Two number is ");
+int g=40; // 1st local variable
+int h=20; // 2nd local variable
+System.out.println(g + " "+ h);
+int div=g/h;
+System.out.println("The div is ="+div);
+
+}
+}
+
+//
+class Addition{
+  public static void main(String[] args) {
+    
+    // declare variables
+    int a = 12, b = 5;
+
+    // addition operator
+    System.out.println("a + b = " + (a + b));
+
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    
+  }
+}
+//
