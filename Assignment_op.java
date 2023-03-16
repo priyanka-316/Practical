e33a7483801a058790b802ca875309d274f81b32
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-16 11:52:37 +0530

    Add files via upload

---
+++ Assignment_op.java
@@ -0,0 +1,9 @@
+class Assignment_op{
+public static void main(String args[]){
+
+int a=10;
+System.out.println(a+=3);
+System.out.println(a-=5);
+System.out.println(a*=2);
+System.out.println(a/=2);
+}}
