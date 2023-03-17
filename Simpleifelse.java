4796371bad6c414ff14092ba71e194f5e6590155
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-17 12:03:29 +0530

    Add files via upload

---
+++ Simpleifelse.java
@@ -0,0 +1,14 @@
+class Simpleifelse{
+public static void main(String args[])
+{
+int x = 5;
+int y = 3;
+   if(x+y < 7)
+     {
+                System.out.println("x+y is less than 7");
+      }
+   else
+    {
+                System.out.println("x+y is greater than 7");
+    }
+}}
