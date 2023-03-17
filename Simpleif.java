19b2b25d7704d99b8c5436c9fc918130a8eb6de2
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-17 12:03:06 +0530

    Add files via upload

---
+++ Simpleif.java
@@ -0,0 +1,15 @@
+// simple if statement
+
+class Simpleif{
+public static void main(String args[])
+{
+int x = 5;
+int y = 3;
+   if(x+y < 7)
+     {
+                System.out.println("x+y is less than 7");
+      }
+    {
+                System.out.println("x+y is greater than 7");
+    }
+}}
