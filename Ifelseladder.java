2245ae1a3a146d5910284a311a431edd16b5c00e
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-17 12:20:14 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,23 @@
+class Ifelseladder{
+public static void main(String args[])
+{
+String name = "Priya";
+if(name == "Meena"){
+System.out.println("Name is meena");
+}
+
+else if(name == "Neha"){
+System.out.println("Name is Neha");
+}
+
+else if(name == "Riya"){
+System.out.println("Name is Riya");
+}
+
+else if(name == "Sonu"){
+System.out.println("Name is Sonu");
+}
+else{
+System.out.println(name);
+}
+}}
