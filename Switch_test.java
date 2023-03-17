f0c58a60333e736bf968c14d7b1bd29312fa1a27
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-17 13:03:31 +0530

    Add files via upload

---
+++ Switch_test.java
@@ -0,0 +1,15 @@
+class Switch_test{
+public static void main(String args[]){
+int a=10;
+switch(a){
+case 0:
+System.out.println("Number is 0");
+break;
+
+case 1:
+System.out.println("Number is 1");
+break;
+default:
+System.out.println(a);
+}
+}}
