cdab0591750f531d2bc76795d74ad651bab84ff4
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-20 12:40:56 +0530

    Add files via upload

---
+++ Whileeg.java
@@ -0,0 +1,9 @@
+class Whileeg1{
+public static void main(String args[]){
+int i=0;
+System.out.println("First 10 even numbers \n");
+while(i%2!=0){
+System.out.println(i);
+i=i+2;
+}
+}}
---
+++ Whileoddnumber.java
@@ -0,0 +1,10 @@
+class Whileoddnumber{
+public static void main(String args[]){
+int i=1;
+System.out.println("First 10 even numbers \n");
+while(i<=20){
+System.out.println(i);
+
+i = i+2;
+}
+}}
