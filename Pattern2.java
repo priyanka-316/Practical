9c28dfabd83e07a982a230e8a02c3a990c115460
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-21 20:38:29 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,33 @@
+/*class Pattern2{
+public static void main(String args[])
+{
+
+for(int i=5;i>=1;i--)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+
+System.out.println();
+}
+}
+}*/
+
+class Pattern2{
+public static void main(String args[])
+{
+int row =5;
+for(int i=5;i>=1;i--)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+
+System.out.println();
+}
+}
+}
+
+
