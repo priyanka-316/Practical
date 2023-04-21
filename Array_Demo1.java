3f559fb363eeb0ab70fb79d71969f5339b47e4e5
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-04-21 10:09:45 +0530

    Add files via upload

---
+++ Array_Demo1.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class Array_Demo1
+{
+	public static void main(String[] args) {
+		int a[] = new int[5]; //declaration and instantiation
+		//initialization
+		a[0]=2;
+		a[1]=9;
+		a[2]=5;
+		a[3]=7;
+		a[4]=1;
+		//traversing(printing one after another) an array using for each loop
+		System.out.println("Traversing an array using for each loop");
+		for(int i:a) 
+			System.out.println(i);
+			
+			System.out.println("Taversing an array buy using for loop");
+			for(int i=0; i<a.length;i++) // length is an one of the property of array 
+			
+			
+			System.out.println(a[i]);
+			
+	}}
+
+
---
+++ Array_demo2.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Array_demo2 {
+
+	public static void main(String[] args) {
+		// declaring and initializing and instanctiation
+		String[] names= {"Mamta","Riya","Saras","Joshan","akshu"};
+		for(int i =0;i<names.length;i++) {
+			System.out.println(names[i]);
+		}
+		System.out.println();
+		int [] phone= {123,321,456,654,951};
+		for(int j=0;j<phone.length;j++)
+			System.out.println(phone[j]);
+	}}
+
+
